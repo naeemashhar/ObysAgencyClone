666336e177e3c3e2cfb7fe4f0ef98ada1aab428e
naeemashhar <naeemashhar98@gmail.com>
2025-04-28 18:05:50 +0530

    Made loader successfully

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+let tl= gsap.timeline();
+
+tl.from(".line h1",{
+    y:150,
+    opacity:0,
+    
+    duration: 0.5,
+    delay : 1,
+    stagger:0.2,
+});
+
+tl.from("#num ",{
+    opacity:0,
+    onStart:function(){
+        let c= document.querySelector("#num h5");
+        let grow=0;
+        setInterval(function(){
+            grow++;
+            if ( grow <= 100 ){
+                c.innerHTML=grow;
+            }
+            else{
+                c.innerHTML=100;
+            }
+        },35);
+
+    },
+});
+
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1,
+})
+
+tl.to("#loader",{
+    opacity:0,
+    duration:0.2,
+    delay:4,
+});
+
+tl.from("#page1",{
+    delay:0.2,
+    y:1200,
+    opacity:0,
+    ease:power4,
+});
+
+tl.to("#loader",{
+    display:"none",
+});
